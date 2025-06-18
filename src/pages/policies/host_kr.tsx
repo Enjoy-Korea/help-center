7f6979aa6012d0e4d1567836c8425871b309ffc7
JISUYOO <jisu.yoo@enkor.kr>
2025-06-18 18:59:31 +0900

    feat: notion embed full content layout

--- src/pages/policies/host_kr.tsx
+++ src/pages/policies/host_kr.tsx
@@ -1,39 +1,12 @@
 import React from "react";
-import Layout from "@theme/Layout";
+import NotionEmbedFullContent from "@site/src/components/NotionEmbedFullContent";
 
 export default function HostKr(): JSX.Element {
   return (
-    <Layout title="호스트 정책" description="호스트 정책">
-      <style>
-        {`
-          .notion-container {
-            width: 100%;
-            height: calc(100vh - 60px - 270px);
-            overflow: hidden;
-            position: relative;
-          }
-          
-          @media (max-width: 997px) {
-            .notion-container {
-              height: calc(100vh - 60px - 582px);
-            }
-          }
-        `}
-      </style>
-      <div className="notion-container" style={{ position: "relative" }}>
-        <iframe
-          src="https://enkorwithus.notion.site/ebd/216d9e29823480c5bd31e39cafd6d0b5"
-          width="100%"
-          height="100%"
-          allowFullScreen
-          style={{
-            display: "block",
-            border: "none",
-            marginTop: "-44px",
-            height: "calc(100% + 44px)",
-          }}
-        />
-      </div>
-    </Layout>
+    <NotionEmbedFullContent
+      src="https://enkorwithus.notion.site/ebd/216d9e29823480c5bd31e39cafd6d0b5"
+      title="호스트 정책"
+      description="호스트 정책"
+    />
   );
 }

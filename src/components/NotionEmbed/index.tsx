0b8c8548706a90f97becc96e59825529cb65171b
JISUYOO <jisu.yoo@enkor.kr>
2025-06-18 19:03:05 +0900

    feat: notion embed component

--- src/components/NotionEmbed/index.tsx
+++ src/components/NotionEmbed/index.tsx
@@ -7,6 +7,29 @@ interface NotionEmbedProps {
   title?: string;
 }
 
+/**
+ * @description Notion 페이지를 임베드하는 컴포넌트
+ * Notion Embed Full Content 컴포넌트와 동일한 기능을 가지고 있지만
+ * 왼쪽에 사이드바 있을 때 사용
+ *
+ * @example
+ * ```mdx
+ * ---
+ * sidebar_position: 1
+ * hide_table_of_contents: true
+ * custom_edit_url: null
+ * ---
+ * import NotionEmbed from "@site/src/components/NotionEmbed";
+ *
+ * <div className="notion-full-width">
+ *  <NotionEmbed
+ *   src={embedSrc}
+ *   height="calc(100vh - 44px)"
+ *   title="FAQ - Notion Page"
+ *  />
+ * </div>
+ * ```
+ */
 export default function NotionEmbed({
   src,
   height = "600px",
